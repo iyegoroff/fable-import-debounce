9b4ff8c5b471924b324b85fdb7c23fefefb8e04d
Igor Yegorov <iegoroff@gmail.com>
2018-09-18 18:52:20 +0300

    version 1.0.0

--- src/Fable.Import.Debounce.fs
+++ src/Fable.Import.Debounce.fs
@@ -2,5 +2,14 @@ module Fable.Import.Debounce
 
 open Fable.Core
 
+type IDebounced<'f> =
+  interface end
+
 [<Import("default", "debounce"); Emit("$0($1, $2, $3)")>]
-let debounce<'f> (_f: 'f) (_interval: int) (_immediate: bool) : 'f = jsNative
+let debounce<'f> (_f: 'f) (_interval: int) (_immediate: bool) : IDebounced<'f> = jsNative
+
+[<Emit("$0")>]
+let unwrap (_debounced: IDebounced<'f>): 'f = jsNative
+
+[<Emit("$0.clear()")>]
+let clear (_debounced: IDebounced<'f>): unit = jsNative
